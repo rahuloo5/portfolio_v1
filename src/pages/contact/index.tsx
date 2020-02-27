36a54afa7e67aea9a68e85c9d956963bbfe1d354
David Obodo <obododavid5@gmail.com>
2020-02-27 09:59:30 +0100

    email copied to clipboard, but without any feedback to user

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,54 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import Layout from '../../layout';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-
-const ContactContainer = styled.div`
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-
-        .contact__header{
-            margin-bottom: 60px;
-        }
-
-        .contact__subheader{
-            font-size: 36px;
-            margin-bottom: 30px;
-            font-weight: 300;
-        }
-
-        .contact__links-wrapper{
-            list-style: none;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-evenly;
-
-            li{
-
-                i{
-                    font-size: 40px;
-                    margin-right: 40px;
-                }
-
-                a{
-                    text-decoration: none;
-                    font-size: 34px;
-                    font-weight: 300;
-                    color: #000000;
-                }
-            }
-        }
-`
+import { ContactContainer } from './style';
 
 const ALL_CONTACTS = [
-    {
-        icon: 'fas fa-envelope',
-        link: 'obododavid5@gmail.com',
-    },
     {
         icon: 'fab fa-github-square',
         link: 'https://github.com/obododavid',
@@ -64,12 +18,26 @@ const ALL_CONTACTS = [
 ]
 
 const Contact = () => {
+
+    const handleCopyToClipboard = (e: any): void => {
+        const element = document.createElement('textarea');
+        element.value = e.target.innerHTML;
+        document.body.appendChild(element);
+        element.select();
+        document.execCommand('copy');
+        document.body.removeChild(element);
+    }
+
     return (
         <Layout isFooterPresent={false} >
             <ContactContainer>
                 <h1 className='contact__header'>contact.</h1>
                 <p className='contact__subheader'>You can contact me through my mail or any of the platforms below:</p>
                 <ul className='contact__links-wrapper'>
+                    <li>
+                        <i className='fas fa-envelope'></i>
+                        <a onClick={handleCopyToClipboard}>obododavid5@gmail.com</a>
+                    </li>
                     {ALL_CONTACTS.map((contact, i) => {
                         const { icon, link } = contact
                         return <li>
