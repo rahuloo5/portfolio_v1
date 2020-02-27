3357e223ca2b561a3bcbdbdc8c2842b35c8952d5
David Obodo <obododavid5@gmail.com>
2020-02-27 07:46:43 +0100

    project page styles completed

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import Layout from '../../layout';
 import { ALL_PROJECTS } from './constants'
+import { Link } from 'react-router-dom'
+
+import { Props } from './IProject';
 
 const ProjectContainer = styled.div`
     height: 100%;
@@ -13,16 +16,20 @@ const ProjectHeader = styled.h1`
     margin-bottom: 30px;
 `
 
-const ProjectBody = styled.div`
+const ProjectBody = styled.div<Props>`
     flex: 1;
     display: flex;
     flex-direction: column;
+    
 
         .projects__body{
             display:grid;
             grid-template-columns: 1fr 1fr 1fr;
             grid-auto-rows: 1fr 1fr 1fr 1fr 1fr;
             flex: 1;
+            opacity: 0;
+            transition: opacity 1s ease-in-out;
+            ${({ displayOne }) => displayOne && 'opacity: 1'};
 
             h4{
                 font-size: 14px;
@@ -42,25 +49,71 @@ const ProjectBody = styled.div`
 
         .projects__buttonContainer{
             margin-bottom: 30px;
+            opacity: 0;
+            transform: translateY(20px);
+            transition-property: opacity, transform;
+            transition: 0.5s ease-in-out;
+            ${({ displayTwo }) => displayTwo && 'opacity: 1; transform: translateY(0px)'};
 
-            button{
-                font-size: 24px;
+            a{
+                font-size: 12px;
                 border: none;
                 color: #074BF8;
                 background-color: transparent;
                 cursor: pointer;
                 font-weight: 600;
                 font-style: italic;
+                position: relative;
+                text-decoration: none;
+    
+                &:after{
+                    content: '';
+                    position: absolute;
+                    width: 100%;
+                    height: 1px;
+                    background-color: #074BF8;
+                    bottom: 0;
+                    left: 0;
+                    transform: scaleX(0);
+                    transform-origin: left;
+                    transition: all 0.25s ease-in-out;
+                }
+    
+                &:hover{
+    
+                    &:after{
+                        transform-origin: right;
+                        transform: scaleX(1);
+                    }
+                }
+            }
+    
+            span{
+                display: inline-block;
+                transform: translate(10px, 0px);
             }
         }
 `
 
-const Projects = () => {
+const Projects: React.FC<Props> = () => {
+    const [displayOne, setDisplayOne] = useState();
+    const [displayTwo, setDisplayTwo] = useState();
+
+    useEffect(() => {
+        setTimeout(() => {
+            setDisplayOne(true);
+        }, 500);
+        setTimeout(() => {
+            setDisplayTwo(true);
+        }, 1000);
+    })
     return (
         <Layout isFooterPresent>
             <ProjectContainer>
                 <ProjectHeader>projects.</ProjectHeader>
-                <ProjectBody>
+                <ProjectBody
+                    displayOne={displayOne}
+                    displayTwo={displayTwo}>
                     <ul className='projects__body'>
                         {ALL_PROJECTS.map((project, i) => {
                             const { title, details, live_link, source_code } = project
@@ -73,8 +126,9 @@ const Projects = () => {
                         })}
                     </ul>
                     <section className='projects__buttonContainer'>
-                        <button>Go to contacts</button>&#10230;
-                </section>
+                        <Link to='/contact'>Go to Contacts</Link>
+                        <span>&#10230;</span>
+                    </section>
                 </ProjectBody>
             </ProjectContainer>
         </Layout>
